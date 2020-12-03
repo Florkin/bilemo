47efdb5d4d2b90a93d84ad87ae882753afcf167d
TristanDiabolo <tristan@diabolo-web.com>
2020-12-03 12:23:28 +0100

    [data/manage] Pictures fixtures + display pictures on manage index

--- src/DataFixtures/ProductFixture.php
+++ src/DataFixtures/ProductFixture.php
@@ -8,6 +8,9 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Liip\ImagineBundle\Imagine\Cache\CacheManager;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class ProductFixture extends Fixture implements DependentFixtureInterface
 {
@@ -15,33 +18,42 @@ class ProductFixture extends Fixture implements DependentFixtureInterface
      * @var BrandRepository
      */
     private $brandRepository;
+    /**
+     * @var CacheManager
+     */
+    private $cacheManager;
 
     /**
      * ProductFixture constructor.
      * @param BrandRepository $brandRepository
+     * @param CacheManager $cacheManager
      */
-    public function __construct(BrandRepository $brandRepository)
+    public function __construct(BrandRepository $brandRepository, CacheManager $cacheManager)
     {
         $this->brandRepository = $brandRepository;
+        $this->cacheManager = $cacheManager;
     }
 
     public function load(ObjectManager $manager)
     {
+        $this->cleanImagesFolders();
+
         $brands = $this->brandRepository->findAll();
         $faker = Factory::create('fr_FR');
 
         for ($i = 0; $i < 60; $i++) {
             $product = new Product();
+            $pictures = $this->fakeUploadPictures();
+
             $product
                 ->setName($faker->words(2, true))
                 ->setDescription($faker->sentences(6, true))
                 ->setPrice($faker->randomFloat(2, 99, 2000))
-                ->setBrand($faker->randomElement($brands));
+                ->setBrand($faker->randomElement($brands))
+                ->setPictureFiles($pictures);
 
             $manager->persist($product);
-
         }
-
         $manager->flush();;
     }
 
@@ -50,6 +62,50 @@ class ProductFixture extends Fixture implements DependentFixtureInterface
         return array(
             BrandFixture::class,
         );
+    }
+
+    private function cleanImagesFolders()
+    {
+        $this->cacheManager->remove();
+        $path = __DIR__ . "/../../public/images/products/*";
+        $files = glob($path);
+        foreach ($files as $file) {
+            if (is_file($file))
+                unlink($file);
+        }
+    }
+
+    private function fakeUploadPictures()
+    {
+        $numberOfImages = $this->randomNumber(1, 4);
+
+        $pictures = [];
+
+        for ($j = 0; $j < $numberOfImages; $j++) {
+            $originalPath = $this->randomPic(__DIR__ . "/FixturesImages");
+            $uniqueName = "img" . $j . ".jpg";
+            $fileSystem = new Filesystem();
+            $targetPath = sys_get_temp_dir() . '/' . $uniqueName;
+            $fileSystem->copy($originalPath, $targetPath, false);
+            $pictures[$j] = new UploadedFile($targetPath, $uniqueName, "image/jpeg", null, true);
+        }
+        return $pictures;
+    }
+
+    function randomPic($dir)
+    {
+        $files = glob($dir . '/*.*');
+        $file = array_rand($files);
+        return $files[$file];
+    }
+
+    private function randomNumber($minNumber, $maxNumber)
+    {
+        try {
+            return random_int($minNumber, $maxNumber);
+        } catch (\Exception $e) {
+            return 1;
+        }
     }
 }
 
